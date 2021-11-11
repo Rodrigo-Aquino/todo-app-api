551aa2611e36945ffabbce4d4fa6f2b475d532e1
Rodrigo Aquino <rodrigovrvr@gmail.com>
2021-11-11 19:42:43 -0300

    adicionado o post nas rotas usuarios e tarefas

--- src/controllers/tarefa-controllers.js
+++ src/controllers/tarefa-controllers.js
@@ -2,5 +2,8 @@ module.exports = (app) => {
     app.get('/tarefa', function (req, res) {
         res.send('Rota ativada com get e recurso tarefa')
       })
+      app.post('/tarefa', function (req, res) {
+        res.send('Rota post de tarefa ativada: tarefa adicionado ao banco de dados')
+      })
 }
 //rodar com npm run dev
--- src/controllers/usuario-controllers.js
+++ src/controllers/usuario-controllers.js
@@ -2,5 +2,9 @@ module.exports = (app) => {
      app.get('/usuario', function (req, res) {
         res.send('Rastreamento da aplicação sendo feita com nodemon')
       })
+
+      app.post('/usuario', function (req, res) {
+        res.send('Rota post de usuario ativada: usuario adicionado ao banco de dados')
+      })
 }
 //rodar com npm run dev
